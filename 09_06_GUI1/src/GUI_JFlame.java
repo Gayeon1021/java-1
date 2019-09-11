86098100fc883369a6fe0cb2367c0c015fd1c7b1
gayeon <s2018w02@e-mirim.hs.kr>
2019-09-11 09:06:48 +0900

    화면 정가운데(한가위) frame 위치하기

--- 09_06_GUI1/src/GUI_JFlame.java
+++ 09_06_GUI1/src/GUI_JFlame.java
@@ -1,22 +1,27 @@
+
+//화면 정가운데((한가위)frame 위치하기
+
 import javax.swing.*;
-import java.awt.*;
+        import java.awt.*;
 
 public class GUI_JFlame {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int 화w = screenSize.width;
+        int 화h = screenSize.height;
+        int 창w = 400;
+        int 창h = 200;
+        frame.setPreferredSize(new Dimension(창w, 창h));
+        frame.setLocation((화w-창w)/2, (화h-창h)/2);
         JPanel panel = new JPanel();
-
-        JLabel label = new JLabel("아싸, 어쩌다 금요일");
-        JButton button = new JButton("오예");
-
+        JLabel label = new JLabel("아싸 어쩌다 금요일");
+        JButton button = new JButton("오예!");
         panel.add(label);
         panel.add(button);
         frame.add(panel);
-
-
-
         frame.pack();
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //X 버튼 누르면 끝
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //X버튼 -> 끝
     }
 }
